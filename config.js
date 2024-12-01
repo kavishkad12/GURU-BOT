6d3f81feb7f884edfaea939e487bf337dc33f141
KD STUDIO <kd1076347@gmail.com>
2024-12-01 12:09:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
+  ['94720552487', 'kdstudio', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
